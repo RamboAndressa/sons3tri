3599893c80a85368e2e1c7d78c5caf5444e2b860
Andressa-rambo <107252397+RamboAndressa@users.noreply.github.com>
2023-10-23 10:43:58 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
-    function tocaSomBuzina(){
-        document.querySelector("som_tecla_buzina").play()
-    }
+function tocaSomBuzina(){
+    document.querySelector("#som_tecla_buzina").play();
+}    
+    const listaDeTeclas = document.querySelectorAll(".tecla");
+    listaDeTeclas[0].onclick = tocaSomBuzina;
+
+function tocaSomAlarme(){
+    document.querySelector("#som_tecla_alarme").play();   
+}
+    const listaDeTeclas = document.querySelectorAll(".tecla");
+    listaDeTeclas[1].onclick = tocaSomAlarme;
