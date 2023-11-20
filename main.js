3ba68b13aa4ff5ff58cff2cbfae4cd5001aaea6c
Andressa-rambo <107252397+RamboAndressa@users.noreply.github.com>
2023-11-20 10:38:54 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
-function tocaSom(idElementoAuio){
-    document.querySelector(idElementoAuio).play();
-}    
-    const listaDeTeclas = document.querySelectorAll(".tecla");
+function tocaSom(idElementoAudio){
+    document.querySelector(idElementoAudio).play();
+}
+const listaDeTeclas = document.querySelectorAll(".tecla");
 
-    let contador = 0;
-    while(contador < listaDeTeclas.length){
-        const efeito = listaDeTeclas[contador].classList[1];
-        const idAuio = "#som_"+efeito;
-        listaDeTeclas[contador].onclick = function (){
-            tocaSom(idAuio)
-        }
-        contador = contador + 1;
+let contador = 0;
+while(contador < listaDeTeclas.length) {
+    const efeito = listaDeTeclas [contador].classList[1];
+    const idAudio = "#som_"+efeito;
+    listaDeTeclas[contador].onclick = function(){
+        tocaSom(idAudio)
     }
+    contador = contador + 1;
+}
